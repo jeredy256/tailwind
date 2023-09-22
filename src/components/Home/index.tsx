2e8b926ace45b583bbbb36d2d7ac2997a1184768
jeredy256 <145425163+jeredy256@users.noreply.github.com>
2023-09-22 00:28:54 -0300

    first commit

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,24 @@
+import Footerr from "../Routes/Footer"
+
+
 export const Home = () =>{
     return(
         <>
-        <h1 className="font-bold bg-emerald-950 m p" >ola fodaser</h1>
+        <div className=" bg-black">
+        <header><h1 className="font-bold
+        bg-red-400 text-center " >ola fodase</h1></header>
+        <p className=" ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo pariatur error saepe libero quod. Laborum libero quis praesentium soluta eveniet. Expedita, aut similique! Blanditiis voluptatibus harum ipsam voluptas adipisci pariatur.
+            Inventore consequuntur dignissimos enim, quidem at vel ipsa, itaque et beatae animi voluptate impedit maiores a possimus excepturi provident iste nemo? Velit, excepturi. Sapiente corporis dolor optio dignissimos, minima ea.
+            Iure natus, vel excepturi est ratione praesentium! Architecto, aliquid nihil. Voluptas aperiam possimus sint! Neque sunt tempore est id, sit repellat fuga aliquam deserunt cumque dignissimos autem inventore sint maxime.
+            Dolor laudantium dolores velit accusamus itaque, amet pariatur aperiam repudiandae maiores voluptatem dolorem. Repellat ipsum quos qui amet recusandae, aspernatur explicabo ipsam eum inventore eos? Dolores optio beatae pariatur laboriosam.
+            Eaque inventore quasi dignissimos, sunt in possimus optio, accusamus iure rerum quo illo animi assumenda fuga ducimus nostrum placeat excepturi error fugiat tempora similique ad eius, labore porro doloremque. Dolorum?
+            Repudiandae quos provident dolorum maxime tempore suscipit cupiditate eligendi necessitatibus quisquam vel? Facilis odio sapiente qui quas et, ipsam natus totam animi distinctio, placeat aliquam adipisci illum praesentium tempora libero!
+            Dolores, explicabo? Cupiditate laudantium totam, deserunt odit veritatis quas eos alias non! Explicabo sapiente illo natus repudiandae voluptates nesciunt harum magnam? Perspiciatis laboriosam, quod officia ullam placeat reiciendis libero nostrum.
+            Numquam, nobis laborum vero ducimus natus atque officia dolore cupiditate sint. Aspernatur itaque tenetur suscipit velit eius dignissimos, rem asperiores aliquid possimus eaque magnam atque ad, quis tempore architecto repellendus!
+        </p>
+        <Footerr  />
+        </div>
         </>
     )
 }
---
+++ src/components/Routes/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+
+const Footerr = () => {
+    return(
+        <>
+        <footer>
+            <h1 className=" bg-red-700 text-end px-5"
+            ><strong>Jeredy</strong></h1>
+        </footer>
+        </>
+    )
+}
+
+export default Footerr 
